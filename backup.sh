9ea50927aa2d2018d6f3e38b6ee5ced2df981f35
kai <kai@mail.com>
2019-11-15 14:21:17 +0100

    Add check if creating archive was successful.

--- backup.sh
+++ backup.sh
@@ -5,15 +5,21 @@ archive='var-www.bak.tar'
 
 if [ $(id -u) -eq 0 ]; then
 
-  # create tar archive
-  tar --create --verbose --file $archive $source
-
-  # compress archive
+  # create tar archive and compress it if successful
+  tar --create --verbose --file $archive $source && \
   xz --compress --verbose $archive
+
+  if [ $? -ne 0 ]; then
+    echo "Something went wrong creating compressed archive. Exiting."
+
+    exit 1 
+  fi
 
 else
 
   echo "This script must be run with root privileges. Exiting."
+
+    exit 1 
 
 fi
 
